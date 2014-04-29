5d8d649d1d6370d2e5f5c51009e2ae85bbf944da
Reamin <satsuki@live.com>
2014-04-30 06:11:11 +0900

    ff

--- Hello/Hello/hello.c
+++ Hello/Hello/hello.c
@@ -1,7 +1,57 @@
-#include <stdio.h>
+//문자A를 초기좌표(10,5)에 출력
+//화살표 이동에 따라 A를 계속해서 출력
+//ESC입력시 종료
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#define X_MAX 79 //열 최대값
+#define Y_MAX 24 //향 최대값
+
+void move_arrow_key(char chr, int *x1, int *y1, int x_b, int y_b);
+void gotoxy(int x, int y);
 
 int main(void){
-	printf("hello world\n");
+	char key;
+	int x = 10, y = 5; // A를 출력할 초기 좌표  
+
+	do{
+		gotoxy(x, y);
+		printf("A");
+		key = getch();
+		move_arrow_key(key, &x, &y, X_MAX, Y_MAX);
+	} while (key != 27); // key의 값이 ESC(아스키코드 27)일때 와일문 탈출
 
 	return 0;
+}
+
+void move_arrow_key(char key, int *x1, int *y1, int x_b, int y_b){
+	switch (key) {
+	case 72: //위쪽(상) 방향의 화살표 키 입력  
+		*y1 = *y1 - 1;
+		if (*y1 < 1)
+			*y1 = 1; // y좌표의 최소값   
+		break;
+	case 75: //왼쪽(좌) 방향의 화살표 키 입력  
+		*x1 = *x1 - 1;
+		if (*x1 < 1)
+			*x1 = 1; // x좌표의 최소값   
+		break;
+	case 77: //오른쪽(우) 방향의 화살표 키 입력  
+		*x1 = *x1 + 1;
+		if (*x1 > x_b)
+			*x1 = x_b; // x좌표의 최대값   
+		break;
+	case 80: //아래쪽(하) 방향의 화살표 키 입력  
+		*y1 = *y1 + 1;
+		if (*y1 > y_b)
+			*y1 = y_b; // y좌표의 최대값   
+		break;
+	default:
+		return;
+	}
+}
+
+void gotoxy(int x, int y){// x=가로위치, y=세로위치 	
+	COORD Pos = { x - 1, y - 1 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
